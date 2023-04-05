b6982d6bd7376bcff0ff77e03eaffa7483685b10
greatmarguinho <128430261+greatmarguinho@users.noreply.github.com>
2023-04-05 20:03:40 -0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,16 @@
+/**
+    Estrutura inicial para um jogo de labirinto
+    versão: 0.1 (Felski)
+*/
+#include <iostream>
+#include <windows.h>
+#include <conio.h>
+#include "funcoes.cpp"
+
+using namespace std;
+
+int main()
+{
+	functionMenu();
+	return 0;
+} //fim mainw
